007a52870f341db41a46a0f08d65b472cbb51c3d
Elias <elias_rrosa@hotmail.com>
2025-02-09 11:04:19 -0300

    feat: h1 and h2 receive attributes rather than extending the props  with htmlprops

--- src/components/text/heading/VinminH1.tsx
+++ src/components/text/heading/VinminH1.tsx
@@ -1,13 +1,14 @@
-import "./VinminH1.css"
+import "./VinminH1.css";
 
-interface VinminH1Props
-  extends React.DetailedHTMLProps<
+interface VinminH1Props {
+  attributes?: React.DetailedHTMLProps<
     React.HTMLAttributes<HTMLHeadingElement>,
     HTMLHeadingElement
-  > {}
+  >;
+}
 
 function VinminH1(props: VinminH1Props) {
-  return <h1 {...props} className="VinminH1"></h1>;
+  return <h1 {...props} className={`VinminH1 ${props.attributes?.className}`}></h1>;
 }
 
 export default VinminH1;
--- src/components/text/heading/VinminH2.tsx
+++ src/components/text/heading/VinminH2.tsx
@@ -1,13 +1,14 @@
 import "./VinminH2.css";
 
-interface VinminH2Props
-  extends React.DetailedHTMLProps<
+interface VinminH2Props {
+  attributes?: React.DetailedHTMLProps<
     React.HTMLAttributes<HTMLHeadingElement>,
     HTMLHeadingElement
-  > {}
+  >;
+}
 
 function VinminH2(props: VinminH2Props) {
-  return <h2 {...props} className="VinminH2"></h2>;
+  return <h1 {...props} className={`VinminH2 ${props.attributes?.className}`}></h1>;
 }
 
 export default VinminH2;
